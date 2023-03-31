e9e67a0093c2ac4c98a05f5da6c3457594e97048
Codie@123 <elliott6518@gmail.com>
2023-03-31 19:26:03 +0530

    Updated by Codie 123
    Mod: Updated the home page with header and hero section .

--- js/swiper.js
+++ js/swiper.js
@@ -7,10 +7,16 @@ const swiper = new Swiper(".swiper", {
   //   pagination: {
   //     el: ".swiper-pagination",
   //   },
-  effect: "coverflow",
-  coverflowEffect: {
-    rotate: 30,
-    slideShadows: false,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      // will set `translateZ(-400px)` on previous slides
+      translate: [0, 0, -400],
+    },
+    next: {
+      // will set `translateX(100%)` on next slides
+      translate: ["100%", 0, 0],
+    },
   },
   // Navigation arrows
   navigation: {
